3492a4fc6f15c59bf3794a08982c271f39108024
SagarMakhar <sagarmakhar@gmail.com>
2021-08-28 04:17:15 +0000

    sm7125-common: Cleanup unused NFC blobs
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sm7125-common/sm7125-common-vendor.mk
+++ sm7125-common/sm7125-common-vendor.mk
@@ -11,7 +11,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/sm7125-common/proprietary/etc/sysconfig/hiddenapi-whitelist-com.motorola.dolby.dolbyui.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-whitelist-com.motorola.dolby.dolbyui.xml \
     vendor/realme/sm7125-common/proprietary/etc/sysconfig/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     vendor/realme/sm7125-common/proprietary/odm/bin/hw/android.hardware.drm@1.3-service.widevine:$(TARGET_COPY_OUT_ODM)/bin/hw/android.hardware.drm@1.3-service.widevine \
-    vendor/realme/sm7125-common/proprietary/odm/bin/opnscr:$(TARGET_COPY_OUT_ODM)/bin/opnscr \
     vendor/realme/sm7125-common/proprietary/odm/etc/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf \
     vendor/realme/sm7125-common/proprietary/odm/etc/init/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
     vendor/realme/sm7125-common/proprietary/odm/etc/init/init.SN100x.nfc.rc:$(TARGET_COPY_OUT_ODM)/etc/init/init.SN100x.nfc.rc \
@@ -21,35 +20,8 @@ PRODUCT_COPY_FILES += \
     vendor/realme/sm7125-common/proprietary/odm/etc/libnfc-nxp-pnscr.conf:$(TARGET_COPY_OUT_ODM)/etc/libnfc-nxp-pnscr.conf \
     vendor/realme/sm7125-common/proprietary/odm/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_ODM)/etc/libnfc-nxp.conf \
     vendor/realme/sm7125-common/proprietary/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/cxd225x_firmware.bin_cxd225x:$(TARGET_COPY_OUT_ODM)/etc/nfc/cxd225x_firmware.bin_cxd225x \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/libnfc-mtp-SN100.conf_206B3:$(TARGET_COPY_OUT_ODM)/etc/nfc/libnfc-mtp-SN100.conf_206B3 \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/libnfc-nci.conf_206B3:$(TARGET_COPY_OUT_ODM)/etc/nfc/libnfc-nci.conf_206B3 \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/nfc_chipset_ref:$(TARGET_COPY_OUT_ODM)/etc/nfc/nfc_chipset_ref \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/nfc_conf_ref:$(TARGET_COPY_OUT_ODM)/etc/nfc/nfc_conf_ref \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/nfc_fw_ref:$(TARGET_COPY_OUT_ODM)/etc/nfc/nfc_fw_ref \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/sn100u.bin_sn100t_fw_01_10_53:$(TARGET_COPY_OUT_ODM)/etc/nfc/sn100u.bin_sn100t_fw_01_10_53 \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/sn100u.bin_sn100t_fw_01_10_57:$(TARGET_COPY_OUT_ODM)/etc/nfc/sn100u.bin_sn100t_fw_01_10_57 \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/st21nfc_fw.bin_st21h:$(TARGET_COPY_OUT_ODM)/etc/nfc/st21nfc_fw.bin_st21h \
-    vendor/realme/sm7125-common/proprietary/odm/etc/nfc/st21nfc_fw7.bin_st21h:$(TARGET_COPY_OUT_ODM)/etc/nfc/st21nfc_fw7.bin_st21h \
     vendor/realme/sm7125-common/proprietary/odm/etc/sap.conf:$(TARGET_COPY_OUT_ODM)/etc/sap.conf \
     vendor/realme/sm7125-common/proprietary/odm/etc/sensor/sensor_config.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/sensor_config.json \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_bootup.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_bootup.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_ese_disableIso.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_ese_disableIso.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_ese_disableIsoRead.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_ese_disableIsoRead.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_nfcon.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_nfcon.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_chr.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_chr.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_cui.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_cui.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ese_3.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ese_3.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ese_aclog.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ese_aclog.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ese_cplc.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ese_cplc.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ese_fabkey.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ese_fabkey.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ese_free.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ese_free.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ese_keyset.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ese_keyset.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ese_sn.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ese_sn.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_ndi.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_ndi.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_pcv.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_pcv.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_spc.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_spc.pnscr \
-    vendor/realme/sm7125-common/proprietary/odm/etc/sn100u_pnx_sst.pnscr:$(TARGET_COPY_OUT_ODM)/etc/sn100u_pnx_sst.pnscr \
     vendor/realme/sm7125-common/proprietary/odm/lib/hw/vendor.oplus.hardware.bluetooth_audio@1.0-impl.so:$(TARGET_COPY_OUT_ODM)/lib/hw/vendor.oplus.hardware.bluetooth_audio@1.0-impl.so \
     vendor/realme/sm7125-common/proprietary/odm/lib/liboemcrypto.so:$(TARGET_COPY_OUT_ODM)/lib/liboemcrypto.so \
     vendor/realme/sm7125-common/proprietary/odm/lib/libpn553_fw.so:$(TARGET_COPY_OUT_ODM)/lib/libpn553_fw.so \
